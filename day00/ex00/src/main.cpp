3a1672359e47a3bf91bb224e0a5cc0f8a2fb6246
lnicosia <lnicosia@student.42.fr>
2019-10-10 13:37:55 +0200

    [ADD] da00 ex00.

---
+++ day00/ex00/src/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 12:33:52 by lnicosia          #+#    #+#             */
+/*   Updated: 2019/10/10 13:36:37 by lnicosia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int		main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] >= 'a' && av[i][j] <= 'z')
+				std::cout << (char)(av[i][j] - 32);
+			else
+				std::cout << av[i][j];
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
