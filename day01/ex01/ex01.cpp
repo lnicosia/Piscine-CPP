aefa01e97f8975526bccaee7e54116c4ccf6d1a2
lnicosia <lnicosia@student.42.fr>
2019-10-11 17:18:48 +0200

    [ADD] day01 ex00 and ex01.

---
+++ day01/ex01/ex01.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 17:15:08 by lnicosia          #+#    #+#             */
+/*   Updated: 2019/10/11 17:18:24 by lnicosia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
+
+//int	main(void)
+//{
+//	memoryLeak();
+//	while(1);
+//	return 0;
+//}
