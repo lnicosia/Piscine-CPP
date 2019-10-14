374849097d2d6ab5111bf8117ad11bb31336b5ad
lnicosia <lnicosia@student.42.fr>
2019-10-14 12:05:35 +0200

    [ADD] ZombieEvent has a default type.

--- day01/ex02/ZombieEvent.cpp
+++ day01/ex02/ZombieEvent.cpp
@@ -6,13 +6,13 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 17:26:51 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/10/11 17:44:12 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/10/14 12:04:49 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ZombieEvent.hpp"
 
-ZombieEvent::ZombieEvent(void)
+ZombieEvent::ZombieEvent(void) : _type("classic")
 {
 	return ;
 }
