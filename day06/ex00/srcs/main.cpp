98fad7d947f437d89a07daa16aa4c187c9155b28
lnicosia <lnicosia@student.42.fr>
2019-10-21 15:54:25 +0200

    [FIX] ex00 Input parsing check function return values: 0 for Integer, 1 for Float, 2 for Double, -1 if invalid.

--- day06/ex00/srcs/main.cpp
+++ day06/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lnicosia <lnicosia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:46:05 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/10/21 15:36:11 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/10/21 15:53:01 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,18 @@ void    print_char(char *str)
     std::cout << "double: " << std::endl;
 }
 
-bool     isLiteral(char *str)
+int     literalType(char *str)
 {
-    size_t  i = 0, period_count = 0;
+    size_t  i = 0, period_count = 0, len = strlen(static_cast<const char *>(str));
     std::string argv = std::string(str);
-    std::string okValues[6] = { std::string("-inf"), std::string("+inf"), std::string("nan"), std::string("-inff"), std::string("+inff"), std::string("nanf") };
-    while (i < 6)
+    std::string floatOkValues[3] = { std::string("-inff"), std::string("+inff"), std::string("nanf") };
+    std::string doubleOkValues[3] = { std::string("-inf"), std::string("+inf"), std::string("nan") };
+    while (i < 3)
     {
-        if (!argv.compare(static_cast<const std::string &>(okValues[i])))
-            return true;
+        if (!argv.compare(static_cast<const std::string &>(floatOkValues[i])))
+            return 1;
+        if (!argv.compare(static_cast<const std::string &>(doubleOkValues[i])))
+            return 2;
         i++;
     }
     i = 0;
@@ -48,26 +51,45 @@ bool     isLiteral(char *str)
     {
         if (str[i] == '.')
             period_count++;
+        else if (str[i] == 'f')
+        {
+            if (i == len - 1 && period_count == 1)
+                return 1;
+            else
+                return -1;
+        }
         else if (!isdigit(static_cast<int>(str[i])))
-            return false;
+            return -1;
         i++;
     }
+    if (period_count == 1)
+        return 2;
     if (period_count > 1)
-        return false;
-    return true;
+        return -1;
+    return 0;
 }
 
 int     main(int ac, char **av)
 {
-    if (ac != 2 || !isLiteral(av[1]))
+    if (ac != 2 || literalType(av[1]) == -1)
     {
         std::cerr << "Please give one literal value" << std::endl;
         return 0;
     }
+    switch (literalType(av[1]))
+    {
+        case 0:
+            std::cout << "Integer" << std::endl;
+            break;
+        case 1:
+            std::cout << "Float" << std::endl;
+            break;
+        case 2:
+            std::cout << "Double" << std::endl;
+            break;
+    }
     size_t  len = 0;
     while (av[1][len])
         len++;
-    if (len == 1)
-        print_char(av[1]);
     return 0;
 }
